78e36e764e0c2388eb21d1473626ed1ebbbb382a
Brian Mai <0hrs3g@gmail.com>
2024-08-31 18:37:17 -0700

    need to use double for decimal precision

---
+++ areal.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double n; 
+    cin >> n;
+    double perimeter = sqrt(n) * 4;
+    cout << fixed << setprecision(6) << perimeter << endl;
+}
